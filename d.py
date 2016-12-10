fb97bfdda682458e22b91233836c5b2cb256a6f7
Noufal Ibrahim <noufal@nibrahim.net.in>
2016-12-10 10:45:50 +0530

    Adds a single loop version

--- d.py
+++ d.py
@@ -5,3 +5,14 @@ def freq(s):
     for alpha in s:
         alpha_count[alpha]+=1
     return alpha_count   
+
+
+# You can avoid an extra loop like this
+def freq(s):
+    alpha_count = {}
+    for alpha in s:
+        if s in alpha_count:
+            alpha_count[alpha] += 1
+        else:
+            alpha_count[alpha] = 1
+    return alpha_count
