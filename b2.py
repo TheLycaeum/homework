f2402f996b5bd48457dfe6359b40a38b8160fa73
Noufal Ibrahim <noufal@nibrahim.net.in>
2016-12-10 10:43:58 +0530

    Adds comment about tuples

--- b2.py
+++ b2.py
@@ -1,8 +1,10 @@
 def breakdown(value):
-    m=()
+    m=() # Don't use a tuple here. Since you're adding to it, use a list
     d=[1000,500,100,50,20,10,1]
     for amount in d:
         num=int(value/amount)
         m+=(amount,)*num
         value-=(amount*num)
         print("{} * {} = {} {}".format(amount,num,num*amount,m))
+        #                                                    ^
+        # You should print the remaining or dispensed amount at the end. Not m
