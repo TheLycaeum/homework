1773f3b820c811a946c2e35bc6f1f65d595bed4e
Noufal Ibrahim <noufal@nibrahim.net.in>
2016-12-10 10:50:03 +0530

    Minor formatting fixes for tables

--- n.py
+++
@@ -1,3 +0,0 @@
-def table(x,y):
-    for number in range(1,y+1):
-        print("{} * {} ={}".format(x,number,x*number))
---
+++ tables.py
@@ -0,0 +1,3 @@
+def table(x,y):
+    for number in range(1,y+1):
+        print("{} * {:2} = {:3}".format(x,number,x*number))
